810a0912c7d0d7c9c727dbb7aa3343075cb960b2
Harshal Deshpande <harshal25012001@gmail.com>
2021-10-24 10:45:14 +0530

    adjusted rate limiter

--- middleware/rateLImiters.js
+++ middleware/rateLImiters.js
@@ -2,14 +2,14 @@ const rateLimit = require("express-rate-limit");
 
 exports.sendLimiter = rateLimit({
     windowMs: 5 * 60 * 1000, // 5 minute
-    max: 30, // Start blocking after 2 requests
+    max: 2, // Start blocking after 2 requests
     message: "Too many OTP requests from this IP, please try again later",
     headers: true
 });
 
 exports.verifyLimiter = rateLimit({
-    windowMs: 2 * 60 * 1000, // 2 minute
-    max: 50, // Start blocking after 2 requests
+    windowMs: 2 * 60 * 1000, // 1 minute
+    max: 5, // Start blocking after 2 requests
     message: "Too many verify requests from this IP, please try again later",
     headers: true
 });
